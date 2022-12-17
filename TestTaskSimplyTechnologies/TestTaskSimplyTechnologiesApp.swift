e3a398328956325d72b55132521b8d4180470520
Hayk Aghavelyan <avaghavelyan@gmail.com>
2022-12-17 19:44:01 +0400

    first commit

---
+++ TestTaskSimplyTechnologies/TestTaskSimplyTechnologiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskSimplyTechnologiesApp.swift
+//  TestTaskSimplyTechnologies
+//
+//  Created by Hayk Aghavelyan on 17.12.22.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskSimplyTechnologiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
